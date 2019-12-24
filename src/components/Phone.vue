fba6eefe4236eb10b954e19aa1b31824c14c1e85
kennysp1n <597939565@qq.com>
2019-12-24 15:41:30 +0800

    增加了移动端样式

---
+++ src/components/Phone.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="phone">
+    <van-nav-bar
+      title="iphone6"
+      left-text="左键"
+      right-text="右键"
+      @click-left="leftDialog=!leftDialog"
+      @click-right="rightDialog=!rightDialog"
+    ></van-nav-bar>
+    <van-grid :column-num="2" v-if="!showOverlay">
+      <van-grid-item v-for="i of csgo" :key="i">
+        <van-image :alt="i" :src="i" @click="imgClick">
+          <template slot="error">加载失败</template>
+        </van-image>
+      </van-grid-item>
+    </van-grid>
+    <van-overlay class-name="my-overlay" :show="showOverlay" @click="showOverlay=!showOverlay">
+      <van-image :src="target"></van-image>
+    </van-overlay>
+    <van-dialog v-model="leftDialog" title="左键" overlay-class="dialog-overlay"></van-dialog>
+    <van-dialog v-model="rightDialog" title="右键" overlay-class="dialog-overlay"></van-dialog>
+  </div>
+</template>
+
+<script>
+import csgo1 from '../assets/csgo1.jpg'
+import csgo2 from '../assets/csgo2.jpg'
+import csgo3 from '../assets/csgo3.jpg'
+import csgo4 from '../assets/csgo4.jpg'
+import csgo5 from '../assets/csgo5.jpg'
+import csgo6 from '../assets/csgo6.jpeg'
+export default {
+  data () {
+    return {
+      csgo: [
+        csgo1, csgo2, csgo3, csgo4, csgo5, csgo6
+      ],
+      showOverlay: false,
+      target: null,
+      leftDialog: false,
+      rightDialog: false,
+    }
+  },
+  methods: {
+    clickLeft () {
+      console.log('456');
+
+    },
+    clickRight () {
+      console.log('456');
+
+    },
+    imgClick (e) {
+      this.showOverlay = !this.showOverlay
+      this.target = e.target.alt
+
+    }
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style lang="scss">
+.phone {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  .my-overlay {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .van-dialog {
+    position: absolute;
+  }
+  .dialog-overlay {
+    position: absolute;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,39 @@
 <template>
   <div class="home">
     <slide-nav />
-    <div class="content"></div>
+    <div class="content">
+      <div class="img-container">
+        <el-image :src="src" alt="iphone6" fit="fill"></el-image>
+        <div class="display-area">
+          <phone class="my-phone"></phone>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import SlideNav from '../components/SlideNav'
+import Phone from '../components/Phone'
 import { mapState, mapMutations } from 'vuex'
+import iphone6 from '../assets/iphone6.jpg'
 export default {
   name: 'home',
   data () {
     return {
-
+      src: iphone6
     }
   },
   computed: {
+
   },
   methods: {
   },
   components: {
-    SlideNav
+    SlideNav, Phone
+  },
+  mounted () {
   }
 }
 </script>
@@ -47,6 +59,27 @@ export default {
     background-color: #ffffff;
     border-radius: 10px;
     box-shadow: 2px 2px 1px rgba(0, 0, 0, 0.1);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .img-container {
+      position: relative;
+      .el-image {
+        z-index: 1;
+        width: 375px;
+        height: 667px;
+      }
+      .display-area {
+        position: absolute;
+        top: 81px;
+        left: 21px;
+        z-index: 2;
+        width: 336px;
+        height: 508px;
+        background-color: white;
+        border: 1px solid black;
+      }
+    }
   }
 }
 </style>
